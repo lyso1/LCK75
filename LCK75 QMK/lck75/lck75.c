4918ab222ab0518fe9f0001754838e21544b4c2d
im a can what do you think lmao? <33337640+lyso1@users.noreply.github.com>
2021-01-09 17:16:06 -0500

    final qmk files for the lck75
    final qmk files for lck75

--- LCK75 QMK/lck75/lck75.c
+++ LCK75 QMK/lck75/lck75.c
@@ -1,4 +1,4 @@
-/* Copyright 2019 Lyso1
+/* Copyright 2020 Lyso1
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
